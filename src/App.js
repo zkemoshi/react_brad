75dc15aec014b6f545036f89b81e3412c057d2d2
Zakaria Moshi <zkemoshi@gmail.com>
2021-07-12 18:24:01 +0300

    expression

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <p>Hello World</p>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    const name = 'John Doe';
+    const loading = true;
+    const showName = true;
+
+    return (
+      <div className='App'>
+        <h1>My App</h1>
+        {loading ? <h1>Hello {showName && name}</h1> : <h4>Loading</h4>}
+      </div>
+    );
+  }
 }
 
 export default App;
