64b3ecba7332c43d0f2106cbc47a3f414f51c174
Zakaria Moshi <zkemoshi@gmail.com>
2021-07-13 14:18:25 +0300

    alert

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from './components/layout/Navbar';
 import axios from 'axios';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 import './App.css';
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // Search github users
@@ -28,17 +30,31 @@ class App extends Component {
   handleClearUsers = () => {
     this.setState({ users: [], loading: false });
   };
+
+  // Set Alloert
+  handleAlert = (msg, type) => {
+    this.setState({
+      alert: {
+        msg,
+        type,
+      },
+    });
+    setTimeout(() => this.setState({ alert: null }), 2000);
+  };
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar title='Efd-Risiti' />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.handleSearchUsers}
             clearUsers={this.handleClearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.handleAlert}
           />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -9,12 +9,17 @@ export class Search extends Component {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
 
   render() {
