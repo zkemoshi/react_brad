8e6a0cea3f007dd84b76450119580ac1085d0cc8
Zakaria Moshi <zkemoshi@gmail.com>
2021-07-13 12:50:53 +0300

    showclear

---
+++ src/components/users/Search.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Search extends Component {
+  state = {
+    text: '',
+  };
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  render() {
+    const { showClear, clearUsers } = this.props;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit} className='form'>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users..'
+            value={this.state.text}
+            onChange={(e) => this.setState({ text: e.target.value })}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear Users
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Search;
