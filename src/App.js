dbc91b12f34905ee333728362a59bb4533670d15
Zakaria Moshi <zkemoshi@gmail.com>
2021-07-13 09:09:04 +0300

    refactor functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+
 import './App.css';
 class App extends Component {
   state = {};
   render() {
     return (
       <div className='App'>
-        <Navbar title='Efd-Risit' />
+        <Navbar title='Efd-Risiti' />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: 'Github Finder',
-    icon: 'fas fa-receipt',
-  };
+const Navbar = (props) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        {' '}
+        <i class={props.icon}></i> {props.title}
+      </h1>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          {' '}
-          <i class={this.props.icon}></i> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fas fa-receipt',
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
